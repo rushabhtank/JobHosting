cb8529070bf7abb8e6554ca5cb143e2165e3bf9f
rushabhtank <rishitank08@gmail.com>
2023-08-14 10:40:24 +0530

    Updates

---
+++ src/main/java/com/myapp/jobhosting/PostController.java
@@ -0,0 +1,14 @@
+package com.myapp.jobhosting;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class PostController {
+
+    public  void redirect(HttpServletResponse responce){
+
+    }
+
+}
